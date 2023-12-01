877ea36d57256934d7ae78c12f48af6d4f828dcb
Thomas Brewer <tom.brewer@mac.com>
2023-12-01 04:56:17 -0500

    fixing some warnings

--- mix.exs
+++ mix.exs
@@ -1,34 +1,36 @@
 defmodule Lua.Mixfile do
   use Mix.Project
 
-  @name      "ExLua"
-  @version   File.read!("VERSION") |> String.strip
-  @github    "https://github.com/bendiken/exlua"
+  @name "ExLua"
+  @version File.read!("VERSION") |> String.trim()
+  @github "https://github.com/bendiken/exlua"
   @bitbucket "https://bitbucket.org/bendiken/exlua"
-  @homepage  @github
+  @homepage @github
 
   def project do
-    [app: :exlua,
-     version: @version,
-     elixir: "~> 1.3",
-     compilers: Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     name: @name,
-     source_url: @github,
-     homepage_url: @homepage,
-     description: description(),
-     aliases: aliases(),
-     deps: deps(),
-     package: package(),
-     docs: [source_ref: @version, main: "readme", extras: ["README.md"]],
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [
-       "coveralls": :test,
-       "coveralls.detail": :test,
-       "coveralls.post": :test,
-       "coveralls.html": :test,
-     ]]
+    [
+      app: :exlua,
+      version: @version,
+      elixir: "~> 1.3",
+      compilers: Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      name: @name,
+      source_url: @github,
+      homepage_url: @homepage,
+      description: description(),
+      aliases: aliases(),
+      deps: deps(),
+      package: package(),
+      docs: [source_ref: @version, main: "readme", extras: ["README.md"]],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
+    ]
   end
 
   def application do
@@ -36,10 +38,12 @@ defmodule Lua.Mixfile do
   end
 
   defp package do
-    [files: ~w(lib mix.exs CHANGES.md README.md UNLICENSE VERSION),
-     maintainers: ["Arto Bendiken"],
-     licenses: ["Public Domain"],
-     links: %{"GitHub" => @github, "Bitbucket" => @bitbucket}]
+    [
+      files: ~w(lib mix.exs CHANGES.md README.md UNLICENSE VERSION),
+      maintainers: ["Arto Bendiken"],
+      licenses: ["Public Domain"],
+      links: %{"GitHub" => @github, "Bitbucket" => @bitbucket}
+    ]
   end
 
   defp description do
@@ -49,12 +53,14 @@ defmodule Lua.Mixfile do
   end
 
   defp deps do
-    [{:credo,       ">= 0.0.0", only: [:dev, :test]},
-     {:dialyxir,    ">= 0.0.0", only: [:dev, :test]},
-     {:earmark,     ">= 0.0.0", only: :dev},
-     {:ex_doc,      ">= 0.0.0", only: :dev},
-     {:excoveralls, ">= 0.0.0", only: :test},
-     {:luerl, "~> 0.3"}]
+    [
+      {:credo, ">= 0.0.0", only: [:dev, :test]},
+      {:dialyxir, ">= 0.0.0", only: [:dev, :test]},
+      {:earmark, ">= 0.0.0", only: :dev},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:excoveralls, ">= 0.0.0", only: :test},
+      {:luerl, "~> 0.3"}
+    ]
   end
 
   defp aliases do
